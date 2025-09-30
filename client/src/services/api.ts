557053e4fe9fcb5db9e319fe64317afa9b2dce27
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-09-30 14:10:53 -0700

    fix: api variable

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api/users"
+  baseURL: import.meta.env.VITE_BASE_URI
 });
 
 export const getUsers = (page = 1, limit = 10, q = "") =>
-  API.get(`?page=${page}&limit=${limit}&q=${q}`);
+  API.get(`/users?page=${page}&limit=${limit}&q=${q}`);
 
-export const getUser = (id: string) => API.get(`/${id}`);
+export const getUser = (id: string) => API.get(`/users/${id}`);
 
-export const createUser = (data: any) => API.post("/", data);
+export const createUser = (data: any) => API.post("/users", data);
 
-export const updateUser = (id: string, data: any) => API.put(`/${id}`, data);
+export const updateUser = (id: string, data: any) => API.put(`/users/${id}`, data);
 
-export const deleteUser = (id: string) => API.delete(`/${id}`);
+export const deleteUser = (id: string) => API.delete(`/users/${id}`);
 
-export const exportUsers = () => API.get("/export", { responseType: "blob" });
+export const exportUsers = () => API.get("/users/export", { responseType: "blob" });
